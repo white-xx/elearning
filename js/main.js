a26401fa45539284cd493b585b9a99521ca93455
white-xx <xdariawhite@gmail.com>
2024-12-09 19:11:10 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -66,18 +66,18 @@
   });
 
   // Header carousel
-  $(".header-carousel").owlCarousel({
-    autoplay: true,
-    smartSpeed: 1500,
-    items: 1,
-    dots: false,
-    loop: true,
-    nav: true,
-    navText: [
-      '<i class="bi bi-chevron-left"></i>',
-      '<i class="bi bi-chevron-right"></i>',
-    ],
-  });
+  // $(".header-carousel").owlCarousel({
+  //   autoplay: true,
+  //   smartSpeed: 1500,
+  //   items: 1,
+  //   dots: false,
+  //   loop: true,
+  //   nav: true,
+  //   navText: [
+  //     '<i class="bi bi-chevron-left"></i>',
+  //     '<i class="bi bi-chevron-right"></i>',
+  //   ],
+  // });
 
   // Testimonials carousel
   $(".testimonial-carousel").owlCarousel({
